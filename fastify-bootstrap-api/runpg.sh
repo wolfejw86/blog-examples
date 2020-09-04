c47e33e68a22b0b3b143c882fa261a492ce0c784
John Wolfe <wolfejw86@gmail.com>
2020-09-03 21:12:49 -0400

    adding basic docker run script

---
+++ fastify-bootstrap-api/runpg.sh
@@ -0,0 +1,6 @@
+ROOT_DIR=$(pwd)
+PG_DATA="$ROOT_DIR/data/postgres"
+
+mkdir -p "$PG_DATA"
+
+docker run -d --name mylocalpg -p 5432:5432 -e POSTGRES_USER=notes_admin -e POSTGRES_PASSWORD=localhost -e POSTGRES_DB=notes_db -e PGDATA=/var/lib/postgresql/data -v $PG_DATA:/var/lib/postgresql/data postgres:11
