ac20488f467f2ef8740627a7e474e1c2503c052d
John Wolfe <wolfejw86@gmail.com>
2020-10-11 17:13:31 -0400

    docker approach

--- fastify-bootstrap-api/plugins/db.js
+++ fastify-bootstrap-api/plugins/db.js
@@ -1,12 +1,35 @@
 const fp = require('fastify-plugin');
 const pgp = require('pg-promise')();
-
+const DbMigrate = require('db-migrate')
 const appConfig = require('../config/appConfig');
 
-module.exports = fp(function (fastify, opts, next) {
+function runMigrations() {
+  return new Promise((resolve, reject) => {
+    const dbMigrate = DbMigrate.getInstance(true);
+    dbMigrate.silence(true);
+
+    dbMigrate.up((error, results = []) => {
+      if (error) {
+        return reject(error)
+      }
+
+      resolve(results)
+    })
+  })
+}
+
+module.exports = fp(async function (fastify, opts) {
   const db = pgp(appConfig.postgresUri);
 
-  fastify.decorate('db', db);
+  fastify.decorate('db', db).addHook('onClose', async (instance, done) => {
+    await db.$pool.end();
+    done();
+  });
+  const migrationResults = await runMigrations();
 
-  next();
+  if (migrationResults.length > 0) {
+    fastify.log.info({ migrationsCount: migrationResults.length, msg: 'Successful migrations run' })
+  }
+
+
 });
--- fastify-bootstrap-api/runpg.sh
+++ fastify-bootstrap-api/runpg.sh
@@ -3,4 +3,4 @@ PG_DATA="$ROOT_DIR/data/postgres"
 
 mkdir -p "$PG_DATA"
 
-docker run -d --name mylocalpg -p 5432:5432 -e POSTGRES_USER=notes_admin -e POSTGRES_PASSWORD=localhost -e POSTGRES_DB=notes_db -e PGDATA=/var/lib/postgresql/data -v $PG_DATA:/var/lib/postgresql/data postgres:11
+docker start mylocalpg || docker run -d --name mylocalpg -p 5432:5432 -e POSTGRES_USER=notes_admin -e POSTGRES_PASSWORD=localhost -e POSTGRES_DB=notes_db -e PGDATA=/var/lib/postgresql/data -v $PG_DATA:/var/lib/postgresql/data postgres:11
